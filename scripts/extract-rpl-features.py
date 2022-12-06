9a8b67d21e93afae66e050d8bca10480aaf5d46d
SungminLee <i.am.sungmin.lee@yonsei.ac.kr>
2022-12-07 02:09:52 +0900

    add features to extract

--- scripts/extract-rpl-features.py
+++ scripts/extract-rpl-features.py
@@ -1,29 +1,60 @@
 #!/usr/bin/env python3
+import argparse
 import csv
+import glob
 import re
 import sys
 
 import coojatrace
 from humanfriendly.tables import format_pretty_table
-
+import subprocess
 
 def write_csv(trace, csv_file, columns, data):
     if trace.is_file(csv_file):
         print(f'Warning: the CSV file "{csv_file}" already exists!', file=sys.stderr)
-    else:
-        with trace.get_log_writer(csv_file) as f:
-            w = csv.writer(f, delimiter=';', quotechar='"')
-            w.writerow(columns)
-            for d in data:
-                w.writerow(d)
+    
+    with trace.get_log_writer(csv_file, overwrite=True) as f:
+        w = csv.writer(f, delimiter=',', quotechar='"')
+        w.writerow(columns)
+        for d in data:
+            w.writerow(d)
 
 
 def main():
-    trace = coojatrace.main()
+    # from plot import plot
+    # import os
+    
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-s', action='store_true', dest='summary', default=False)
+    # parser.add_argument('input', type=str, nargs="+")
+
+    
+    # try:
+    #     conopts = parser.parse_args()
+    # except Exception as e:
+    #     sys.exit(f"Illegal arguments: {str(e)}")
+    # print(conopts.input)
+    # import sys
+    # if sys.platform.startswith("win") and "*" in conopts.input:
+    #     conopts.input = glob.glob(conopts.input)
+    
+    # if not isinstance(conopts.input, list):
+    #     conopts.input = [i for i in conopts.input]
+    # conopts.input = [i for i in conopts.input if os.path.isdir(i)]
+    
+    
+    # for input in conopts.input:
+        
+    trace, simulation_logdir = coojatrace.main()
 
     network_events = trace.get_events(event_type='network', description='steady-state')
     network_stable_time = network_events[0].time if network_events else 0
 
+    network_script = trace.get_script(description='attack')
+    network_attck_time = network_script[0].time if network_script else 0
+
+    # network_stable_time = 0 # From the start
+    trace.get_events()
     motes = {}
     data = []
     p = re.compile(r'.*DATA: (.+)$')
@@ -33,26 +64,49 @@ def main():
     for o in trace.get_mote_output(start_time=network_stable_time):
         m = p.match(o.message)
         if m:
-            row = [o.time, o.mote_id] + [int(v) for _k, v in (g.split(':') for g in m.group(1).split(','))]
+            row = [o.time, o.mote_id] + [int(v) for _k, v in (g.split(':') for g in m.group(1).split(','))] 
             last = motes[o.mote_id] if o.mote_id in motes else None
             motes[o.mote_id] = row
             # Use relative counters since last known statistics instead of aggregated
             if last:
                 row = row[:5] + [x - y for x, y in zip(row[5:], last[5:])]
             else:
-                row = row[:5] + [0] * 6
-            data.append(row)
+                pass
+                # row = row[:5] + [0] * 6
+            if (o.time < network_attck_time)|(network_attck_time == 0):
+                row = row + ['No']
+            elif o.time > network_attck_time:
+                row = row + [network_script[0].description.split(' ', 1)[0]]
+            else:
+                row = row + ['Start']
 
+            data.append(row)
+    
     # Print 20 first values
-    column_names = ['Time', 'Mote', 'Seq', 'Rank', 'Version', 'DIS-R', 'DIS-S', 'DIO-R', 'DIO-S',
-                    'DAO-R', 'RPL-total-sent']
+    column_names = ['Time', 'Mote', 
+                    'Seq', 'Rank', 'Version', 
+                    'DIS-UR', 'DIS-MR', 'DIS-US', 'DIS-MS', 
+                    'DIO-UR', 'DIO-MR', 'DIO-US', 'DIO-MS', 
+                    'DAO-R', 'DAO-S', 'DAOA-R', 'DAOA-S', 'dio_intcurrent','dio_counter', 
+                    'Attack']
     print(format_pretty_table(data[:20], column_names))
     if len(data) > 20:
         print(f"Only showing 20 first rows - remaining {len(data) - 20} rows not shown.")
 
     # Save statistics to CSV file
-    write_csv(trace, 'rpl-statistics.csv', column_names, data)
+    csv_name = 'rpl-statistics.csv'
+    write_csv(trace, csv_name, column_names, data)
+    
+    
+    m = re.search(r'udp-\d+.*$', simulation_logdir)
+    # sim_name = m.group()
+    
+    # plot(os.path.join(simulation_logdir, csv_name), sim_name)
 
 
 if __name__ == '__main__':
     main()
+    
+    
+    
+    
